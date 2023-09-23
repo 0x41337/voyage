0d9e1943c96a45b8914e8b6a2feb71f7bc936baf
0x41337 <041337@proton.me>
2023-09-23 07:46:31 -0300

    Changing VE_ALL_HOOKS to use integers instead of null pointers

--- include/voyage.h
+++ include/voyage.h
@@ -1,18 +1,18 @@
 /*
   Voyage A library for creating and managing hooks for unix systems authored by Gabriel.
-  <https://github.com/0x41337/Voyage>     <041337@proton.me>
+  <https://github.com/0x41337/voyage>     <041337@proton.me>
 
   Voyage is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
-  the Free Software Foundation, either VEersion 3 of the License, or
-  (at your option) any later VEersion.
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
 
   Voyage is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without eVEen the implied warranty of
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU General Public License for more details.
 
-  You should haVEe receiVEed a copy of the GNU General Public License
+  You should have received a copy of the GNU General Public License
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 #pragma once
@@ -45,20 +45,19 @@ typedef uint64_t addr64_t;
 typedef void *dummy_func_t;
 
 // Constants
-#define VE_VERSION  0.1;
-#define VE_ALL_HOOKS nullptr;
+#define VE_VERSION 0.1;
+#define VE_ALL_HOOKS 0x00001;
 
 /// @brief Search by symbol address
 /// @param img_name Image/module name
 /// @param sym_name Symbol name
 /// @param dest Destination that will receive the found address
 /// @return `VE_STATUS` can be [`VE_ERROR_MODULE_NOT_FOUND`, `VE_ERROR_SYMBOL_NOT_FOUND` and `VE_OK`]
-VE_STATUS VE_FindSymbolAddress(const char* img_name, const char* sym_name, void* dest);
+VE_STATUS VE_FindSymbolAddress(const char *img_name, const char *sym_name, void *dest);
 
-
-/// @brief Create a hook 
+/// @brief Create a hook
 /// @param address The address of function
-/// @param hook_function The detour function 
+/// @param hook_function The detour function
 /// @param original_function  Pointer to original function
 /// @return `VE_STATUS` can be [`VE_ERROR_ALREADY_CREATED`, `VE_ERROR_NOT_CREATED` and `VE_OK`]
-VE_STATUS VE_CreateHook(void* address, dummy_func_t hook_func, dummy_func_t *original_function);
+VE_STATUS VE_CreateHook(void *address, dummy_func_t hook_func, dummy_func_t *original_function);
